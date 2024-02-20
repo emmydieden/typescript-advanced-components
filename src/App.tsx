90fec565e44bab8a3163e0090e8e8441de1ae661
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2024-02-20 10:54:12 +0100

    building polymorphic component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import Input from "./components/Input";
+import Container from "./components/Container";
 import Button from "./components/Button";
 
 function App() {
   return <main>
-    <p>
-      <Button el="button">A button</Button>
-    </p>
-    <p>
-      <Button el="anchor" href="https://google.com">A link</Button>
-    </p>
+   <Container as={Button}>Click me</Container>
   </main>
 }
 
