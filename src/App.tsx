6043ba67f38ee8ba7a66e2e1522a78c72a95624a
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2024-02-20 10:34:02 +0100

    building wrapper components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Input from "./components/Input";
+import Button from "./components/Button";
+
 function App() {
-  return <h1>Let's get started!</h1>;
+  return <main>
+    <p>
+      <Button el="button">A button</Button>
+    </p>
+    <p>
+      <Button el="anchor" href="https://google.com">A link</Button>
+    </p>
+  </main>
 }
 
 export default App;
